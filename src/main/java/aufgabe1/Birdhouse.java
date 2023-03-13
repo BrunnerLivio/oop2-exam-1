9c14e0f189d7dc50afab85fa30633e371d8ec8cb
Livio Brunner <livio.brunner.lb1@gmail.com>
2023-03-13 13:05:18 +0100

    add birdhouse

--- src/main/java/aufgabe1/Birdhouse.java
+++ src/main/java/aufgabe1/Birdhouse.java
@@ -1,18 +1,48 @@
 package aufgabe1;
 
+import aufgabe1.bird.Bird;
 import aufgabe1.food.Food;
+import aufgabe1.food.Water;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Birdhouse {
-    Map<Food, Integer> foodToAmountMap = new HashMap<>();
+  Map<Food, Integer> foodToAmountMap = new HashMap<>();
 
-    public void addFood(Food food, int amount) {
-        foodToAmountMap.put(food, amount);
+  public void addFood(Food food, int amount) {
+    foodToAmountMap.put(food, amount);
+  }
+
+  public Map<Food, Integer> getFoodToAmountMap() {
+    return foodToAmountMap;
+  }
+
+  private Food removeFood(Food food) {
+    int foodAmount = foodToAmountMap.get(food);
+
+    if (foodAmount >= 1) {
+      foodToAmountMap.put(food, foodAmount - 1);
+      return food;
     }
 
-    public Map<Food, Integer> getFoodToAmountMap() {
-        return foodToAmountMap;
+    return null;
+  }
+
+  Food visit(Bird bird) {
+    Food eatenPreferredFood = null;
+
+    for (Food preferredFood : bird.getPreferredFoods()) {
+      for (Food birdhouseFood : foodToAmountMap.keySet()) {
+        if (birdhouseFood.equals(preferredFood) && eatenPreferredFood == null) {
+          eatenPreferredFood = removeFood(birdhouseFood);
+        }
+      }
     }
+
+    if (eatenPreferredFood == null) {
+      return new Water();
+    }
+    return eatenPreferredFood;
+  }
 }
