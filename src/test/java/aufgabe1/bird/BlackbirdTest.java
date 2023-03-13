04aa86729cf832506294a3e12e5044fb34026afe
Livio Brunner <livio.brunner.lb1@gmail.com>
2023-03-13 13:14:15 +0100

    test cases black bird

--- src/test/java/aufgabe1/bird/BlackbirdTest.java
+++ src/test/java/aufgabe1/bird/BlackbirdTest.java
@@ -1,7 +1,41 @@
 package aufgabe1.bird;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import aufgabe1.food.Food;
+import aufgabe1.food.Nut;
+import aufgabe1.food.Seed;
+import aufgabe1.food.Worm;
+
 class BlackbirdTest {
 
-    // TODO Test für `Blackbird` schreiben (keine Logik von `Bird` testen!)
+  @Test
+  void testEatsPreferredFood() {
+    // given
+    Blackbird blackbird = new Blackbird();
 
+    // when
+    boolean eatenSeed = blackbird.eat(new Seed());
+    boolean eatenWorm = blackbird.eat(new Worm());
+
+    // then
+    assertTrue(eatenSeed);
+    assertTrue(eatenWorm);
+  }
+
+  @Test
+  void testDoesntEatNotPreferredFood() {
+     // given
+     Blackbird blackbird = new Blackbird();
+
+     // when
+     boolean eatenNut = blackbird.eat(new Nut());
+ 
+     // then
+     assertFalse(eatenNut);
+  }
 }
